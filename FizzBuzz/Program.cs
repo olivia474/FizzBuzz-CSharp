02fd310f4934ebcc7a2e60b1dbd197dffdfd7d7e
olivia474 <olivia.lengthorn@bp.com>
2023-10-11 16:19:08 +0100

    FizzBuzz and extra rules

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -2,13 +2,77 @@
 
 internal class Program
 {
-    static void Fizzbuzz()
-    {
-        // Your code goes here
-    }
-    
     static void Main(string[] args)
     {
         Fizzbuzz();
     }
+    static void Fizzbuzz()
+    {
+        for(int i = 1; i <= 300; i++) {
+        
+        List<string> result = new List<string>();
+
+        if(IsDivisibleBy(i, 3))
+        {
+            result.Add("Fizz");
+        }
+        
+        if (IsDivisibleBy(i, 5))
+        {
+            result.Add("Buzz");
+        }
+
+        if(IsDivisibleBy(i, 7))
+        {
+            result.Add("Bang");
+        }
+        
+        if(IsDivisibleBy(i, 11))
+        {
+            result.Clear();
+            result.Add("Bong");
+        } 
+        
+        if(IsDivisibleBy(i, 13))
+        {
+            bool inserted = false;
+            
+            for(int j = 0; j < result.Count; j++)
+            {
+                
+                if (result[j].StartsWith('B'))
+                {
+                result.Insert(j, "Fezz");
+                inserted = true;
+                break;
+                }
+            }
+
+                if(!inserted)
+                {
+                    result.Add("Fezz");
+                }
+        }
+        
+        if(IsDivisibleBy(i, 17))
+        {
+            result.Reverse();
+        }
+
+        if(result.Count == 0)
+        {
+            Console.WriteLine(i);
+        }
+
+        else
+        {
+            Console.WriteLine(string.Join("", result));
+        }
+
+       }
+     }
+     private static bool IsDivisibleBy(int numerator, int denominator)
+     {
+        return numerator % denominator == 0;
+     }
 }
